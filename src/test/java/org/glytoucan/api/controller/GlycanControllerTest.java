663226f20f9b2102ab1791b95431f377cfd48665
aoki <aokinobu@gmail.com>
2016-06-22 13:54:03 +0900

    new test cases

--- src/test/java/org/glytoucan/api/controller/GlycanControllerTest.java
+++ src/test/java/org/glytoucan/api/controller/GlycanControllerTest.java
@@ -115,8 +115,39 @@ public class GlycanControllerTest {
     String strResult = encodeToString(img, "png");
     logger.debug("strResult:>" + strResult);
     logger.debug("<html><img src=\"data:image/png;base64," + strResult + "\"></html>");
-    Assert.assertEquals("iVBORw0KGgoAAAANSUhEUgAAAVYAAACGCAIAAAAJhDVPAAAHw0lEQVR42u2dP0ijZxzHn3KcWmtb4dojwntcwLSNJeVssZChUAeHDLY4WOggRwoZbsjgQQYpoeQgg4MHERwsdHCwQ0GslAyBEwJCoeDg4OBww42ODg5CHezzXtrTmuRJ8r7P+7zPm3w+vIO+nOa5+Pw+z5+87/sVVwAwwAjeAgAUAAAoAABQAACgAABAAQCAAgAABQAACgAAFAAAKAAAUAAAoAAAQAEAgAIAAAUAAAoAABQAACgAAFAAAKAAAEABAIACAAAFAAAKAAAUAAAoAABQAACgAADoIwUIHZh8Lw4PD4vF4tzcXOOlU6lUJpMpl8snJyd0FEAB3hTwt7/DkALq9bpIvSOcYfFDTFQS4rdPxe8p8euUeD4pvr8v7t+VLjg6OqK7AAroNwVcXl4WCgUxMSSexcVfX7Q+/vxcFB6Ie3fX1tboMYAC+kcBsv7F1+Mi/Z548aht/b85/vhMTI3m83k6DaCAPlGArGfx1fvuIN+x/htHfVpagLkAoIB+UEC1WnUX/7Kqu6z//+YCsViMfQFAAdFWgLsEmHzb3e3rqf4bR+FBJpOh6wAKiLACarWa+xGAh/p/vTvoOA6fFAIKiLACcrmceOp4VIA8vrlXLpfpPYACoqqAZDLpfuzvWQHFh6wFAAVEWAHiLdHzRuDNYz2RSqXoPYACAldAgHiuf3n88onjOPQeQAFRnQWMjIyIAx+zgI2P5FKC3gMoIKoK+LeFnhXwU3xubo7eAyhgUBXw3YfFYpHeAyjAFgUo7i9WnPGsgMnJycPDQ3oPoACjCmhX571+e62Anz/2ooBn8aGhoUqlcnFxQQcCFGBUAc1l3PJ11Tq4/j1Toz3cI9Q4XjyKx+Pb29vZbNZxHEQAKCAyCrj5440vFhcXxbcf9HRpcCaTKRQKjR9/+fIlIgAUEI4Cuqn/jgo4Pz9Pp9OuBbqZC9Sn5+fnFxYWLi8vb74EIgAUYFQBiimAuv5bPphQWkDOBWZmZjrsCzyfTCQS+Xz+Vv0jAkABIcwCrpT7/Ioz7c5vbW3JFb774NAfH7oPDmxcNVSfdu8jeOrImUIqlapWqx3/y4gAUMCVeuD18wThjvt8Xa4XWp6XFbuzs7O0tCRHe/faQSHGxsaSyWQul6vVau0Gf0QAKMBgW9vv8zWLQ1H/Zh5PjggABQSlgG5q2HAGASIAFGBCAebzRbpZ5nQ0DiIANT2tNJkFWL0qCUgEliQd2ZwQFbn0qjccHR1NT0+HZQEyBc0pwJsIrEo6svoxMNFJr7o1/ieTyRCfTI8CTCugexFYmHSEArRTq9WkAlgIRFgBfiaQChHYmXSEArSzsrJSKpXC7MkUs//FZxAisDPpCAVoR/7pV1dXUcAgWkMhgt3dXTuTjlCAdnK53ObmJgpg++BaBI8fP7Y26QgFaEdOAcLNqkUBIdR/x/0ha5OOUEAQ24EzMzMoYFAU0M3nzzYnHaEA7Zyfn4+NjYV4wRgKMLoL0Px1MzYnHaGAIJidnd3b20MBg7UQUD31xOKkIxQQBJVKJZvNogAUcKMr+0s6CjaFyeaEKKvbpmJ0dJQLhAfIAuqFgM1JR8wCAiKdTu/v76OAgdgO6LgdmEi8vhHAyqQjFBAQ5XL5yZMnKABclpaW3OeXhZ101LIeLCkzm9vmjZOTk3g8jgLAZWdnR3z5rmcFyEmE/6SjdlMVG8rM5rb5YXx8/NWrVyhg0Dk7O5Nj+J07dzwnHclVpbcFyK0x1kyZ2dw2w8jlm7Q/Chjo4i+VSrFYbHl5eX193XPSUb1e97YN2fyPzSjA2rYZplAohHK/EAqwq/hPT08bJ30mHaGAyCkgn8+HctcwCrCu+BtoSTrqpnLaTcvbPcfdfEJUWG0zTDab3djYQAEU//8soCXpyNsTDUzOAuxsm0kuLi7kIi6UxHoUYGPx38R/0pG3Z5wZXghY2DaTyC4R1v2CKMDe4r85RPhJOvJQZu126U0mRIXeNpMdQ/aK4+NjFDCIxW/m6doeRlpD/c/ithnuIaH9CajPEEf+7u8d0lJm9gSxRKJtAwJvukXT/uAUYG2B2dw2FKDtrxuVtZmuxB7Pa/6gZwF2KoAi7G8FRGOHVldij7fiZyQEFBCaAnQl9nge+W+JgB4JKMCcArQk9vgvfmbFgALCUYDPxB4tIz8KABQQjgKq1arnxJ6DgwONIz97AYACTCvAXQL4SOwZHh72X/wAKCA0BfhM7JmYmGj5SeGtIZ2BHVCApQoIKLGHy10BBURDAQEl9qAAQAHRUEBAiT0oAFCAZgUEiL/EHsdxFApghx9QgNWzgOASe25tByICQAH2XhpkZ2IPAAqwXgGaEnsAUIAeBSiusVOcCTexBwAF6Eme6fXbawXoS+wBQAEmFNBcxi1fV62D69+jL7EHAAXYroDm1AqNiT0AKMCoArqp/44K0JjYA4ACDClAMQVQ13/LBxPqSuwBQAHmZgFXyn1+xZl25/0n9gCggJYKMJQ8o57wd6MGn4k9ACgg+La23+drFoei/rluFyDaCugpix4A+kcBDOAAzAIAYCAVAAAoAABQAACgAABAAQCAAgAABQAACgAAFAAAKAAAUAAAoAAAQAEAgAIAAAUAAAoAABQAACgAAAUAAAoAABQAACgAAFAAAKAAAEABANDH/AOrjeRMhFhN6AAAAABJRU5ErkJggg==", strResult);
+    Assert.assertEquals("iVBORw0KGgoAAAANSUhEUgAAAVYAAACGCAIAAAAJhDVPAAAHjUlEQVR42u2dvWsjRxiHJ5iznYvzAZccMuxxAimJHBROCQ6oCKRRocIJLhxIIYwCKlX4QMVxiKADFS58YIObQAoXThEQDgQVgisEhkBAhQoXLvwHuHThQhAVzuwpOTu2NFrtx+ys9DxscV7O1tqa95kP7c5PXAHADCP4EwCgAABAAQCAAgAABQAACgAAFAAAKAAAUAAAoAAAQAEAgAIAAAUAAAoAABQAACgAAFAAAKAAAEABAIACAAAFAAAKAAAUAAAoAABQAACgAABAAQCAAgAABQDAFClA+IHOv0Wn06lWq7lcbvDS6XQ6n8/X6/XT01MaCqAAdwr429uhSQHtdluk3xHWgvgxJnaT4rfPxO9p8euKeJkQPzwUD+9JF3S7XZoLoIBpU0C/369UKmJ5XryIi7++HH78+YWoPBIP7u3s7NBiAAVMjwJk/YtvPhDZ98SrJyPr/83xx+di5X65XKbRAAqYEgXIehZfv2938mPrf3C0M9ICjAUABUyDAprNpj35l1XtsP7/GwvEYjHWBQAFRFsB9hQg8ba92jdR/Q+OyqN8Pk/TARQQYQW0Wi37IwAX9f96ddCyLD4pBBQQYQWUSiXx1HKpAHl8+6Ber9N6AAVEVQGpVMr+2N+1AqqPmQsACoiwAsRbYuKFwJvHXjKdTtN6AAUEroAAcV3/8vjlU8uyaD2AAqI6ClhcXBTHHkYB+x/LqQStB1BAVBXw7xW6VsBP8VwuR+sBFDCrCvj+o2q1SusBFGCKAhTPFyvOuFZAIpHodDq0HkABWhUwqs4n/fJaAT9/4kYBL+Lz8/O7u7u9Xo8GBChAqwLulvHQ11Xr4PrnrNyf4BmhwfHqSTwePzw8LBaLlmUhAkABkVHAzW8f/GNjY0N89+FEtwbn8/lKpTL49rOzM0QAKCAcBTip/7EKuLy8zGaztgWcjAXambW1tfX19X6/f/MlEAGgAK0KUAwB1PU/dGNCaQE5FlhdXR2zLvAykUwmy+XyrfpHBIACQhgFXCnX+RVnRp0/ODiQM3x749Dnj+2NAwd3DbUz9nMETy05Ukin081mc+yvjAgABVypO14vOwiPXedzOF8Yel5WbKPRKBQKsre37x0UYmlpKZVKlUqlVqs1qvNHBIACNF7r6HW+u+JQ1L+e7ckRAaCAoBTgpIY1ZxAgAkABOhSgP1/EyTRnrHEQAaiZaKbJKMDoWUlAIjAk6cjkhKjIpVe9odvtZjKZsCxApqA+BbgTgVFJR0ZvAxOd9Kpb/X8qlQpxZ3oUoFsBzkVgYNIRCvCdVqslFcBEIMIK8DKAVIjAzKQjFOA7z549q9VqYbZkitn75DMIEZiZdIQCfEe+9dvb2yhgFq2hEMHR0ZGZSUcoYArbJGVp1PKBFMHm5qaxSUcoILheIayPJFBACPU/dn3I2KQjFBBE/Y/6EgVMoQKcyN7kpCMUEFz9h2UBFBDC+61+m01OOkIBGgaGKGAmJgKqXU8MTjpCASgAgleA56SjYFOYTE6IMvraJniYFQVMuQXUEwGTk44YBTAKAK/LAWPXe5LJ1w8CGJl0hAJYDoTAKRQK9v5lYScdDW2IhpSZydfmxQJ8KAg2jUZDfPWuawXIQYT3pKNRbdGEMjP52jyODbk1CK4uLi5kHz43N+c66SibzbqbgNzqY/WUmcnXphk5fZP2RwEzXfy1Wi0Wi21tbe3t7blOOmq32+6WIe/+Zz0KMPbaNFOpVEJ5XggFmFX85+fng5Mek45QQOQUUC6XQ3lqGAUYV/wDfEk6clI5innp0H3c9SdEhXVtmikWi/v7+yiA4v+fBXxJOnK3o4HOUYCZ16aTXq8nJ3GhJNajABOL/ybek47c7XGmeSJg4LXpRDYJ6fpQXhoFmFv8N7sIL0lHLsps1Cq9zoSo0K9NZ8OQreLk5AQFzGLx69ld20VPq6n9GXxtmltIaG8B9Rliz+/82SFfysycIJZIXNuMwB/doGF/cAowtsBMvjYU4Nu7G5W5mV+JPa7n/EGPAsxUAEU43QqIxgqtX4k97oqfnhBQQGgK8Cuxx3XPf0sEtEhAAfoU4Etij/fiZ1QMKCAcBXhM7PGl50cBgALCUUCz2XSd2HN8fOxjz89aAKAA3QqwpwAeEnsWFha8Fz8ACghNAR4Te5aXl4d+Uuhwi1gAFBCyAgJK7OF2V0AB0VBAQIk9KABQQDQUEFBiDwoAFOCzAgLEW2KPZVkKBbDCDyjA6FFAcIk9t5YDEQGgAHNvDTIzsQcABRivAJ8SewBQgD8KUNxjpzgTbmIPAArwJ3lm0i+vFeBfYg8ACtChgLtlPPR11Tq4/jn+JfYAoADTFXA3tcLHxB4AFKBVAU7qf6wCfEzsAUABmhSgGAKo63/oxoR+JfYAoAB9o4Ar5Tq/4syo894TewBQwFAFaEqeUQ/4najBY2IPAAoI/lpHr/PdFYei/rlvFyDaCpgoix4ApkcBdOAAjAIAYCYVAAAoAABQAACgAABAAQCAAgAABQAACgAAFAAAKAAAUAAAoAAAQAEAgAIAAAUAAAoAABQAACgAAAUAAAoAABQAACgAAFAAAKAAAEABADDF/ANcSa/n0pN5CwAAAABJRU5ErkJggg==", strResult);
   }
+  
+  @Test
+  public void testImageG15021LG() throws Exception {
+    String accessionNumber = "G15021LG";
+
+    logger.debug("accessionNumber:>" + accessionNumber);
+    ResponseEntity<byte[]> response = gc.getGlycanImage(accessionNumber, "png", "cfg", "extended");
+    byte[] result = response.getBody();
+    BufferedImage img = ImageIO.read(new ByteArrayInputStream(result));
+
+    String strResult = encodeToString(img, "png");
+    logger.debug("strResult:>" + strResult);
+    logger.debug("<html><img src=\"data:image/png;base64," + strResult + "\"></html>");
+    Assert.assertEquals("iVBORw0KGgoAAAANSUhEUgAAAIYAAABSCAIAAAAetDv/AAAC10lEQVR42u3asYvaUBwH8G83B/+AQA8q1EFoBuGEujdQoVIcHB1CyVRCpwxSMjg4OHjDgctRKN7ucEMKdsrQMYODg8MN3W9QOOEEHa4/Ynu0BalGzUtz3y9vD3mf/N7vvae4ZxIWcApIwpCEJAxJSMKQhCQMSRiSkIQhCUkYkpCEIQlJGJIwJCEJQxKSMCQhCUMShiQkYUhCkhRmtVo9FpIgCFzXNQwDYXRdr1Qq7XZ7Mpkkx2M0GhWLRVUq8ZH4vg+8BJ4DHwEPGAPXwAi4Aj4AT8VG5iIJ9VEoFLrdbpqrRF7ScRzgGXAJLDeMO+Ac0BTOxTrD4VBI0rxwybsBb4HXwM1mj4fxHTi1bVshSbPZbLVaaW7vMr/Am7AIltuNqagorBXTNDudTmpJPM8Lm8d0a4+ftaJpWhL6StpIwiXrRdi9l7uPc+n2ambkz6SKRPpkuMVaRhp3Jycn8e+M/2JQonLE51mWBZxFJZFhynlFoYcqlSM+TLaS4bEjMsmnmNeuTVOfHhLgye6N/ffxRc72JDn4OrDcY3xDjHkUJJlMBrjdg+SrLH2skkMmn8+HF1mRST4bhsH2fsg0Gg3gYg+S967rchN8yAwGA+BVZBIpsiAIeFQ8ZBaLRS6XA/xIJJflclntxYYsm/JVpe2Oq9/vA6e73Dmux41Y+r6vlsRxHCX3j0cvzHq9Drzb6SpFTogyHcqv/2zbVnJLf3SS+XwuS1Cosk2tTKvVaq1WU/gL0kNM0+z1eikkWatIrZRKpX/1lStp6fJtJsFj3QjV7C9ie5L0FXnJ8I8QF+F55fbXD1Yj4EwqSdd1z/PukxFZsuQbUvJoxPzpyR5GzitSDeHZHtlsVo7olmUNh8MkFMc6s9lM07TxeJx+kv8ooqLq0SRJXEhCEoYkJGFIQhKGJCRhSMKQhCQMSUjCkIQkDElIwpCEIQlJGJKQhCEJSRiSMCQhCbNTfgBhmNNUvEd3pQAAAABJRU5ErkJggg==", strResult);
+  }
+  
+  @Test
+  public void testImageG00029MO() throws Exception {
+    String accessionNumber = "G00029MO";
+
+    logger.debug("accessionNumber:>" + accessionNumber);
+    ResponseEntity<byte[]> response = gc.getGlycanImage(accessionNumber, "png", "cfg", "extended");
+    byte[] result = response.getBody();
+    BufferedImage img = ImageIO.read(new ByteArrayInputStream(result));
+
+    String strResult = encodeToString(img, "png");
+    logger.debug("strResult:>" + strResult);
+    logger.debug("<html><img src=\"data:image/png;base64," + strResult + "\"></html>");
+    Assert.assertEquals("iVBORw0KGgoAAAANSUhEUgAAAVYAAACGCAIAAAAJhDVPAAAHo0lEQVR42u2dvUtkVxiHT5DVzcZ8wCbLCHdRcJLMhglrggGLQBqLKUzYwkAKEQOWU7gwhSzD4oLFFi4o2ARSWJgiIAbCFANbDAiBgIWFhYV/wJYWFguxMOc6bNY4M2fux7nnvnPv83ALnZX1Hef+nvMxd+6rLgEgxyj+BAAoAABQAACgAABAAQCAAgAABQAACgAAFAAAKAAAUAAAoAAAQAEAgAIAAAUAAAoAABQAACgAAFAAAKAAAEABAIACAAAFAAAKAAAUAAAoAABQAACgAABAAQCQdQUcHh7W6/XZ2Vl1RblcrlQq6+vrJycnqf8tJNcGMPAKaLVaqvye8kbUzwW1WVS/f6H+KKvfHqgXk+qne+reLZ23o6OjVP4KkmsDGHgFXFxc1Go1NTasnk2ov7/ufvz1lardV3dvbWxsuHz+kmsDyIICdMbUdx+pmQ/Uy4c9M/bf8eeX6sGdarXqLP9iawPIiAJ0ZtS3H/oDad+MtY/WlE6am/FWcm0AWVBAo9HwF9g6OQEz9ma8LRQKSa+9JdcGkAUF+NPsyXf9HbVQGWsftfuVSiWftQFkRAHNZtPfZo+QsasdOM/zkns3TnJtABlRwPLysnrsRYyZPr6/u76+nsPaADKigFKp5L+1Hjlm9fHk5tuSawPIiALUOyr0Ztv1Y6tYLpdzWBtAVhSgiZwxffz6uUqUeLV5nsfZAyjAxO3bt9VBjJF2+1M9Xc9hbQAZUUCxeHWxfeSYPZ2YnZ1N8JkLrg0gCwpYWFhQT8ajx+zHT+r1eoLPXHBtAFlQwN7envrm/cgx05OIw8PDEPsOb+j8p84f87+OUdvk5GTw2gByqoDXr19PTEyoXz6LErNnEzMzM8HzH+TbGy6IU9vw8PDm5qZ+gpxAgAJM7OzsqAd3QnwOp328fKjz2Wq1zEN954DfNepdvRCztt3d3aWlJc/zEAGggD7Mz8+rHz4OdfltpVKp1WoBsx1KATcWCzFrOz09RQSAAvpwfn6up/R+0oKMt62pubm5R48eXVxchFVA172A6wro/NpKbYgAUEB/C+jxdnp6us/a+8VksVisVqtd8x9wFnAj6n03CGzVhggABfTfF9CraP/mnE/G/ffk21fmtKb8a/Ufe3o0LpfLjUbDVGu3Id38Y30VYKs2RAAooD86FXt7ewsLC3pE9a/PU2p0dLRUKi0vLzebzV4DbK/Zfq99wV5bgGaDxK8NEQAKSO9pdFwXYNgsMMwgLIIIAAWkLAUJZSACQAGZVUyQixQQAQQh1EoTBYibZQScccQRgZBOR1Y+pZ3D2swcHR1NTU2lZQEU4E4B0UQgqtPRVUz+iXeoHNZmHv9LpVKKd6ZHAa4VEFwEAjsdoQDrNJtNrQAWAgOsgDgTSIMIZHY6QgHWWV1dXVtbS/NMJszxF59JiEBmpyMUYB390j9//hwF5NEaBhHs7+/L7HSEAjJ4ThJLUdsHWgSLi4tiOx2hgORGhbTekkABKeS/7/6Q2E5HKCCJ/Pf6FgVkUAFBZC+50xEKSC7/aVkABaTweptfZsmdjlCAg4khCsjFQsD0YWfJXZhQAAqAxBUgvAtT7JjltTYV4eaXKCCbFjAvBCR3OmIWwCwA4m4H9N3vkdyFCQWwHQiJI6QLU9cTUUjMJNcWxwK8KQg+LrswhRqghMRMcm0x54ZcGgSXZ2dnegwfGhqy2IUp7Blm2KyyHjPJtTlGL9+0/VFArsO/trZWKBRWVla2trbsdmEKPs803HwxIQWIrc0xtVotlc8LoQBZ4X/16lX7wSS6MKEAyQqoVqupfGoYBYgLf5skujAFyZj5XlqJKkBabY5ZWlra3t5GAYT/fxaw2IUp7G6Ty1mAzNpc0m7DnUrHehQgMfzXsdiF6TLMZSeOFwICa3OJPiW061P51ShAbvivDxFxOh1FiFmvXXrrd+mVXJvLE0OfFcfHxyggj+F3c3ftCCOto/NPcG2Oz5DUXgLymeLIH/yzQ1ZiluJAN4i15QT+6IKm/ckpQGzAJNeGAqy9uoOyNrPVsSfymj/pWYBMBRDCbCtgMHZobXXsiRZ+RkJAAakpwFbHnsgj/w0RcEYCCnCnACsde+KHn1kxoIB0FBCzY4+VkR8FAApIRwGNRiNyx56DgwOLIz97AYACXCvAXwLE6NgzMjISP/wAKCA1BcTs2DM2Ntb1ncKAt4gFQAEpKyChjj1c7gooYDAUkFDHHhQAKGAwFJBQxx4UACjAsgISJF7HHs/zDApghx9QgOhZQHIde25sByICQAFyLw2S2bEHAAWIV4Cljj0AKMCOAgzX2BkeSbdjDwAKsNN5Juy3bxVgr2MPAApwoYDOGHf9vWYdvP1/7HXsAUAB0hXQ2bXCYsceABTgVAFB8t9XARY79gCgAEcKMEwBzPnvemNCWx17AFCAu1nApXGf3/BIr8fjd+wBQAFdFeCo84x5wh9EDTE79gCggORr7b3P1ykOQ/65bhdgsBUQqhc9AGRHAQzgAMwCACCXCgAAFAAAKAAAUAAAoAAAQAEAgAIAAAUAAAoAABQAACgAAFAAAKAAAEABAIACAAAFAAAKAAAUAIACAAAFAAAKAAAUAAAoAABQAACgAADIMP8CkFquIwCD058AAAAASUVORK5CYII=", strResult);
+  }  
+  
 
   public static String encodeToString(BufferedImage image, String type) {
     String imageString = null;
@@ -182,13 +213,13 @@ public void testG00032MOGlycoct() throws Exception {
 }
 
 
-@Test
-public void testWurcsSubstructure() throws Exception {
-  String url = "/glycans/sparql/substructure?sequence=WURCS%3D2.0%2F2%2C2%2C1%2F%5Ba1122h-1x_1-5%5D%5Ba2112h-1x_1-5_2*NCC%2F3%3DO%5D%2F1-2%2Fa%3F-b1&format=wurcs";
-      given().redirects().follow(false).when().get(url).then().statusCode(HttpStatus.SC_OK).and().body(containsString("http://rdf.glycoinfo.org/glycan/wurcs/2.0/monosaccharide/a1122h-1x_1-5"));
-}
+//@Test
+//public void testWurcsSubstructure() throws Exception {
+//  String url = "/glycans/sparql/substructure?sequence=WURCS%3D2.0%2F2%2C2%2C1%2F%5Ba1122h-1x_1-5%5D%5Ba2112h-1x_1-5_2*NCC%2F3%3DO%5D%2F1-2%2Fa%3F-b1&format=wurcs";
+//      given().redirects().follow(false).when().get(url).then().statusCode(HttpStatus.SC_OK).and().body(containsString("http://rdf.glycoinfo.org/glycan/wurcs/2.0/monosaccharide/a1122h-1x_1-5"));
+//}
 
-@Test
+//@Test
 public void postImage() {
   String sequence = "RES\\n"
     + "1b:x-dglc-HEX-x:x\\n"
@@ -210,16 +241,16 @@ String image = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAABSCAIAAAB/iU
 
 @Test
 public void testImage() throws Exception {
-  String sequence = "RES\\n"
-    + "1b:x-dglc-HEX-x:x\\n"
-    + "2b:b-dgal-HEX-1:5\\n"
-    + "3b:a-dgal-HEX-1:5\\n"
-    + "4b:b-dgal-HEX-1:5\\n"
-    + "5s:n-acetyl\\n"
-    + "LIN\\n"
-    + "1:1o(4+1)2d\\n"
-    + "2:2o(3+1)3d\\n"
-    + "3:3o(3+1)4d\\n"
+  String sequence = "RES\n"
+    + "1b:x-dglc-HEX-x:x\n"
+    + "2b:b-dgal-HEX-1:5\n"
+    + "3b:a-dgal-HEX-1:5\n"
+    + "4b:b-dgal-HEX-1:5\n"
+    + "5s:n-acetyl\n"
+    + "LIN\n"
+    + "1:1o(4+1)2d\n"
+    + "2:2o(3+1)3d\n"
+    + "3:3o(3+1)4d\n"
     + "4:4d(2+1)5n";
 String output2 = "{\"encoding\":\"glycoct\",\"structure\":\"" + sequence + "\"}"; 
     
